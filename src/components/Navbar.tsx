16f3eaab3576f9ac3246bd1581d5c73902c61591
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-07-30 13:45:49 +0200

    Add nav links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,31 @@
-import React from "react";
+type NavLinks = {
+  name: string;
+  target: string;
+};
+
+export const navLinks: NavLinks[] = [
+  { name: "Calculator", target: "calculator" },
+  { name: "How it Works", target: "how-it-works" },
+  { name: "About", target: "about" },
+];
 
 const Navbar = () => {
   return (
-    <div className="position-fixed h-16 bg-c-primary flex items-center text-c-background">
-      <div className="flex items-center max-w-content">
-        <p>Hello</p>
-        <p>Hello</p>
-        <p>Hello</p>
+    <div className="position-fixed h-16 flex justify-center bg-c-grey-one">
+      <div className="flex w-full max-w-content justify-between items-center">
+        <p>Clamp Calculator</p>
+        <div className="flex w-full max-w-80 justify-between ">
+          {navLinks.map((link) => (
+            <a
+              key={link.name}
+              href={link.target}
+              className="hover:text-c-primary"
+            >
+              {link.name}
+            </a>
+          ))}
+        </div>
+        <p>Theme Switcher</p>
       </div>
     </div>
   );
