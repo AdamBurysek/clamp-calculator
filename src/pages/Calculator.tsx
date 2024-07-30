6640f9607ab2fb343bbcc2a37fe18e788966e544
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-07-30 18:02:58 +0200

    Add heading styles

--- src/pages/Calculator.tsx
+++ src/pages/Calculator.tsx
@@ -1,8 +1,8 @@
 const Calculator = () => {
   return (
-    <div className="w-full text-center pt-10">
-      <h1 className="text-3xl font-bold underline text-c-primary">
-        Hello world!
+    <div className="w-full text-center pt-4">
+      <h1 className="bg-gradient-to-r from-c-text from-18% via-c-primary via-48% to-c-text to-68% inline-block text-transparent bg-clip-text">
+        Your Trusted Clamp Calculator
       </h1>
     </div>
   );
