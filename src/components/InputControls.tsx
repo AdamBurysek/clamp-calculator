13e3a3a6163933d3fd4cadefffe0648fd841bc98
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-08-01 16:40:39 +0200

    Move input controls to separate component

---
+++ src/components/InputControls.tsx
@@ -0,0 +1,47 @@
+import ToggleUnitButton from "./common/ToggleUnitButton";
+
+const InputControls = () => {
+  return (
+    <div className="flex  gap-8 p-4 ">
+      <div className="flex flex-col items-center gap-8 w-96 h-36 bg-c-secondary border border-c-background rounded-2xl drop-shadow-box">
+        <div className="flex items-center gap-6 mt-2 h-12">
+          <h3 className="pb-1">Target Values</h3>
+          <ToggleUnitButton />
+        </div>
+        <div className="flex gap-4 ">
+          <div className="flex gap-2">
+            <p className="text-c-text font-bold text-xl">MIN</p>
+            <span className="flex gap-2 px-2 items-center bg-c-primary text-c-background rounded-md">
+              <input
+                type="number"
+                aria-label="Min Target Value"
+                className="w-14 text-center bg-c-primary text-c-background font-bold"
+                min={1}
+                max={1201}
+              />
+              <p className="text-c-background font-bold">px</p>
+            </span>
+          </div>
+          <div className="flex gap-2">
+            <p className="text-c-text font-bold text-xl">MAX</p>
+            <span className="flex gap-2 px-2 items-center bg-c-primary text-c-background rounded-md">
+              <input
+                type="number"
+                aria-label="Min Target Value"
+                className="w-14 text-center bg-c-primary text-c-background font-bold"
+                min={1}
+                max={1201}
+              />
+              <p className="text-c-background font-bold">px</p>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="w-96 h-36 bg-c-secondary border border-c-background rounded-2xl drop-shadow-box">
+        <h3>Viewport Range</h3>
+      </div>
+    </div>
+  );
+};
+
+export default InputControls;
--- src/pages/Calculator.tsx
+++ src/pages/Calculator.tsx
@@ -1,6 +1,6 @@
-import { GlobalStateProvider } from "../context/GlobalStateContext";
+import InputControls from "../components/InputControls";
 import RemControls from "../components/RemControls";
-import ToggleUnitButton from "../components/common/ToggleUnitButton";
+import { GlobalStateProvider } from "../context/GlobalStateContext";
 
 const Calculator = () => {
   return (
@@ -12,45 +12,7 @@ const Calculator = () => {
         <div className="flex flex-col items-center bg-c-secondary rounded-2xl ">
           <h2 className="pt-2">Calculator Settings</h2>
           <RemControls />
-          <div className="flex  gap-8 p-4 ">
-            <div className="flex flex-col items-center gap-8 w-96 h-36 bg-c-secondary border border-c-background rounded-2xl drop-shadow-box">
-              <div className="flex items-center gap-6 mt-2 h-12">
-                <h3 className="pb-1">Target Values</h3>
-                <ToggleUnitButton />
-              </div>
-              <div className="flex gap-4 ">
-                <div className="flex gap-2">
-                  <p className="text-c-text font-bold text-xl">MIN</p>
-                  <span className="flex gap-2 px-2 items-center bg-c-primary text-c-background rounded-md">
-                    <input
-                      type="number"
-                      aria-label="Min Target Value"
-                      className="w-14 text-center bg-c-primary text-c-background font-bold"
-                      min={1}
-                      max={1201}
-                    />
-                    <p className="text-c-background font-bold">px</p>
-                  </span>
-                </div>
-                <div className="flex gap-2">
-                  <p className="text-c-text font-bold text-xl">MAX</p>
-                  <span className="flex gap-2 px-2 items-center bg-c-primary text-c-background rounded-md">
-                    <input
-                      type="number"
-                      aria-label="Min Target Value"
-                      className="w-14 text-center bg-c-primary text-c-background font-bold"
-                      min={1}
-                      max={1201}
-                    />
-                    <p className="text-c-background font-bold">px</p>
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div className="w-96 h-36 bg-c-secondary border border-c-background rounded-2xl drop-shadow-box">
-              <h3>Viewport Range</h3>
-            </div>
-          </div>
+          <InputControls />
         </div>
       </div>
     </GlobalStateProvider>
