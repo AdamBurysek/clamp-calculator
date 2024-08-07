d020e429eebeebaea9563e6cee9c081a7695adc7
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-08-07 09:24:34 +0200

    Change path to index

--- src/routes.tsx
+++ src/routes.tsx
@@ -10,7 +10,7 @@ const routes = [
     element: <Layout />,
     children: [
       {
-        path: "/",
+        index: true,
         element: (
           <GlobalStateProvider>
             <Calculator />
