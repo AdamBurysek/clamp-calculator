46ab74f3fb33b7bedd18e0aa6d92d50fb364858c
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-08-08 08:14:35 +0200

    Add target options array

--- src/components/AdditionalControls.tsx
+++ src/components/AdditionalControls.tsx
@@ -5,9 +5,18 @@ import OptionInput from "./common/OptionInput";
 const AdditionalControls = () => {
   const { outputInPx, setOutputInPx } = useGlobalState();
 
+  const targetOptions = [
+    { label: "Font size", value: "font-size" },
+    { label: "Width", value: "width" },
+    { label: "Padding left", value: "padding-left" },
+    { label: "Padding right", value: "padding-right" },
+    { label: "Margin left", value: "margin-left" },
+    { label: "Margin right", value: "margin-right" },
+  ];
+
   return (
     <div>
-      <OptionInput />
+      <OptionInput options={targetOptions} />
       <CheckboxInput
         onChange={() => setOutputInPx(!outputInPx)}
         checked={!outputInPx}
--- src/components/common/OptionInput.tsx
+++ src/components/common/OptionInput.tsx
@@ -1,4 +1,8 @@
-const OptionInput = () => {
+interface Props {
+  options: { label: string; value: string }[];
+}
+
+const OptionInput = ({ options }: Props) => {
   return (
     <div className="flex gap-2 my-2">
       <label htmlFor="clamp-option">Target</label>
@@ -7,13 +11,11 @@ const OptionInput = () => {
         name="clamp-option"
         id="clamp-option"
       >
-        <option value="none">None</option>
-        <option value="font-size">Font size</option>
-        <option value="width">Width</option>
-        <option value="padding-left">Padding left</option>
-        <option value="padding-right">Padding right</option>
-        <option value="margin-left">Margin left</option>
-        <option value="margin-right">Margin right</option>
+        {options.map((option, index) => (
+          <option key={index} value={option.value}>
+            {option.label}
+          </option>
+        ))}
       </select>
     </div>
   );
