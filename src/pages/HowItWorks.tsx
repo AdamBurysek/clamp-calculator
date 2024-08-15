bf2fe32a304fed99cd155bdee65fb3259bbd2ccf
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-08-15 19:10:00 +0200

    Add clamp graph

--- src/pages/HowItWorks.tsx
+++ src/pages/HowItWorks.tsx
@@ -1,5 +1,14 @@
+import { ClampGraph } from "../components/Icons";
+
 const HowItWorks = () => {
-  return <h1>Coming soon</h1>;
+  return (
+    <div className="flex w-full flex-col items-center">
+      <h1 className="py-4 text-c-grey-nine">How Clamp function Works</h1>
+      <div className="h-auto max-h-[280px] w-full max-w-[544px]">
+        <ClampGraph className="h-full w-full" />
+      </div>
+    </div>
+  );
 };
 
 export default HowItWorks;
