dabc8de56d3d2f41f01bec9056c32fb07a407806
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-08-19 06:38:49 +0200

    Code refactoring

--- src/components/RemControls.tsx
+++ src/components/RemControls.tsx
@@ -25,6 +25,8 @@ const RemControls: React.FC = () => {
   const [localRemBase, setLocalRemBase] = useState<number>(remBase);
 
   const updateValues = (oldRemBase: number, newRemBase: number) => {
+    setRemBase(newRemBase);
+    setLocalRemBase(newRemBase);
     setMinTargetValue(recalculateRem(oldRemBase, newRemBase, isTargetUnitsPx, minTargetValue));
     setMaxTargetValue(recalculateRem(oldRemBase, newRemBase, isTargetUnitsPx, maxTargetValue));
     setMinViewportValue(
@@ -35,50 +37,43 @@ const RemControls: React.FC = () => {
     );
   };
 
-  const changeRemBase = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     const oldRemBase = remBase;
     const type = e.currentTarget.dataset.type;
     if (type === "increment" && remBase <= 1201) {
       const newRemBase = remBase + 1;
-      setRemBase(newRemBase);
-      setLocalRemBase(newRemBase);
       updateValues(oldRemBase, newRemBase);
     } else if (type === "decrement" && remBase > 1) {
       const newRemBase = remBase - 1;
-      setRemBase(newRemBase);
-      setLocalRemBase(newRemBase);
       updateValues(oldRemBase, newRemBase);
     }
   };
 
-  const handleRemInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value === "" ? "" : parseInt(e.target.value, 10);
     if (value === "" || (!isNaN(value) && value >= 0 && value <= 1201)) {
       setLocalRemBase(value as number);
     }
   };
 
-  const handleRemInputBlur = () => {
+  const handleInputBlur = () => {
     const oldRemBase = remBase;
     const newRemBase = localRemBase;
 
     if (newRemBase === null || newRemBase < 1) {
-      setRemBase(1);
       updateValues(oldRemBase, 1);
     } else if (newRemBase > 1201) {
-      setRemBase(1201);
       updateValues(oldRemBase, 1201);
     } else {
-      setRemBase(newRemBase);
       updateValues(oldRemBase, newRemBase);
     }
   };
 
-  const handleRemInputKeyDown = useHandleInputKeyDown();
+  const handleInputKeyDown = useHandleInputKeyDown();
 
   return (
     <div className="my-4 flex gap-4">
-      <StepButton type="decrement" onClick={changeRemBase} />
+      <StepButton type="decrement" onClick={handleButtonClick} />
       <div className="rounded-2xl bg-c-primary">
         <p className="px-6 pt-2 text-xl font-bold text-c-background">
           1 rem =
@@ -87,9 +82,9 @@ const RemControls: React.FC = () => {
             aria-label="Rem Base Value"
             name="Rem Base Value"
             value={localRemBase}
-            onChange={handleRemInputChange}
-            onBlur={handleRemInputBlur}
-            onKeyDown={handleRemInputKeyDown}
+            onChange={handleInputChange}
+            onBlur={handleInputBlur}
+            onKeyDown={handleInputKeyDown}
             className="w-14 bg-c-primary text-center text-c-background"
             min={1}
             max={1201}
@@ -97,7 +92,7 @@ const RemControls: React.FC = () => {
           px
         </p>
       </div>
-      <StepButton type="increment" onClick={changeRemBase} />
+      <StepButton type="increment" onClick={handleButtonClick} />
     </div>
   );
 };
