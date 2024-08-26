de8acd7934566248d9f45b180b052cf4400cf3f3
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-08-26 07:03:52 +0200

    Header change

--- src/pages/Calculator.tsx
+++ src/pages/Calculator.tsx
@@ -10,7 +10,7 @@ const Calculator = () => {
     <div className="flex w-full max-w-content flex-col items-center">
       <span className="bg-c-background">
         <h1 className="inline-block bg-gradient-to-r from-c-grey-nine from-20% via-c-primary via-85% to-c-grey-nine to-50% bg-clip-text py-4 text-transparent">
-          Easy Clamp Calculation Tool
+          CSS Clamp Calculation Tool
         </h1>
       </span>
       <div className="max-s:scale-75">
