65bbdabdf4365e61e931d75a3ea7ce1f0fff27d2
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-08-29 09:15:28 +0200

    Add more seo keywords

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -10,7 +10,14 @@ const Seo = () => {
         guide, clamp function in CSS, calculate min-max font sizes, fluid design with CSS clamp, CSS
         dynamic scaling, CSS clamp formula, responsive layout calculator, flexible design with
         clamp, CSS clamp for padding, generate responsive values with clamp, modern CSS design,
-        clamp calculator for web developers, clamp() calculator
+        clamp calculator for web developers, clamp() calculator, Tailwind CSS clamp utility,
+        Tailwind CSS responsive design, Tailwind CSS scaling values, generating clamp values with
+        Tailwind CSS, Tailwind CSS clamp function, Tailwind CSS dynamic typography, responsive
+        design with Tailwind CSS, Tailwind CSS utilities calculator, using Tailwind CSS for dynamic
+        layouts, Tailwind CSS min-max font size calculation, Tailwind CSS fluid design techniques,
+        modern web design with Tailwind CSS and clamp, Tailwind CSS clamp padding, Tailwind CSS
+        clamp calculator, Clamp calculator with comments, clamp function explanation, CSS clamp
+        function with comments
       </p>
     </div>
   );
